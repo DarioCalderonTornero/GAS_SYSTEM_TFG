316c17ab67cea00d9027c6d253f8428d465b5cee
DarioCalderonTornero <152275116+DarioCalderonTornero@users.noreply.github.com>
2026-02-18 09:35:02 +0100

    Compile error fixed + MyGameplayAbilty

---
+++ Source/GAS_SYSTEM_TFG/GAS_SYSTEM/MyAbilities/MyPlayerGameplayAbility.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "MyPlayerGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_SYSTEM_TFG_API UMyPlayerGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+public: 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
+	bool bShouldShowAbilityBar = false;
+		
+	
+};
