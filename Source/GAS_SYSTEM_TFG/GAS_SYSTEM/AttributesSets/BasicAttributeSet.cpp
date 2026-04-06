b071f912ceef4e0b4237f29803bc606918175c71
DarioCalderonTornero <152275116+DarioCalderonTornero@users.noreply.github.com>
2026-04-06 19:09:24 +0200

    Hit Reaction 100%

--- Source/GAS_SYSTEM_TFG/GAS_SYSTEM/AttributesSets/BasicAttributeSet.cpp
+++ Source/GAS_SYSTEM_TFG/GAS_SYSTEM/AttributesSets/BasicAttributeSet.cpp
@@ -46,9 +46,14 @@ void UBasicAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallb
 	{
 		SetHealth(GetHealth());	
 		
-		FGameplayTagContainer HitReactionTagContainer;
-		HitReactionTagContainer.AddTag(FGameplayTag::RequestGameplayTag(FName("GameplayAbility.HitReaction")));
-		GetOwningAbilitySystemComponent()-> TryActivateAbilitiesByTag(HitReactionTagContainer);
+		if (Data.EffectSpec.Def->GetAssetTags().HasTag(FGameplayTag::RequestGameplayTag("Effect.HitReaction")))
+		{
+			FGameplayTagContainer HitReactionTagContainer;
+			HitReactionTagContainer.AddTag(FGameplayTag::RequestGameplayTag(FName("GameplayAbility.HitReaction")));
+			GetOwningAbilitySystemComponent()-> TryActivateAbilitiesByTag(HitReactionTagContainer);
+		}
+		
+		
 	}
 	else if (Data.EvaluatedData.Attribute == GetStaminaAttribute())
 	{
